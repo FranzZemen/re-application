937fa6d431376b799d301a4be755296bd30df43e
Franz Zemen <franzzemen@gmail.com>
2022-10-15 17:09:50 -0400

    npmu

--- ts-src/scope/application-scope.ts
+++ ts-src/scope/application-scope.ts
@@ -2,9 +2,12 @@ import {ExecutionContextI} from '@franzzemen/app-utility';
 import {Scope} from '@franzzemen/re-common';
 import {RuleSetScope} from '@franzzemen/re-rule-set';
 import {ApplicationOptions} from './application-options.js';
+import {ApplicationParser} from '../parser/application-parser.js';
 
 export class ApplicationScope extends RuleSetScope {
+  static ApplicationParser = 'ApplicationParser';
   constructor(options?: ApplicationOptions, parentScope?: Scope, ec?: ExecutionContextI) {
     super(options, parentScope, ec);
+    this.set(ApplicationScope.ApplicationParser, new ApplicationParser());
   }
 }
